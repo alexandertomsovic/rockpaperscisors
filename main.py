dd930b889fbebec8b4ef88985b166adaf09a7556
Alexander R. Tomsovic <84757117+alexandertomsovic@users.noreply.github.com>
2021-10-21 09:45:56 -0700

    Update main.py

--- main.py
+++ main.py
@@ -4,28 +4,28 @@ import click                  # For clearing the screen after every game
 def clrscr():
                               # Clear screen using click.clear() function
     click.clear()
-print(elements.welcome)       # To import game Heading from elements.py
+print(elements.welcome_element)       # To import game Heading from elements.py
 play="Y"
 while play=="Y" or play=="y": # The Game will keep running until this while statement breaks
   choice=int(input("Enter Your Choose:\nType 0 for Rock , 1 for Paper , 2 for scissors:\n)> "))
   if choice==0 or choice==1 or choice==2:
     print("Your choice:")
     if choice==0:
-      print(elements.rock)
+      print(elements.rock_element)
     elif choice==1:
-      print(elements.paper)
+      print(elements.paper_element)
     elif choice==2:
-      print(elements.scissors)
+      print(elements.scissors_element)
     else:
       print("Kindly enter an Valid Number 0 , 1 , 2 only")  # Checks if the input was valid 
     print("Computer's choice :")
     choice_computer=random.randint(0,2)  # Random Function to make a random choice between 0,1,2             
     if choice_computer==0:
-      print(elements.rock)
+      print(elements.rock_element)
     elif choice_computer==1:
-      print(elements.paper)
+      print(elements.paper_element)
     elif choice_computer==2:
-      print(elements.scissors)
+      print(elements.scissors_element)
     string_choice=str(choice)
     string_choice_computer=str(choice_computer)
     num=string_choice+string_choice_computer
